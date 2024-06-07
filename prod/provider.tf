6213935d4bd059782e1ccfe3365bafdbac819c7e
Tim Gerritsen <timgerritsen@outlook.com>
2024-06-07 22:23:53 +0000

    Fixed vars and removed DIMM-TEST01

--- prod/provider.tf
+++ prod/provider.tf
@@ -16,6 +16,11 @@ variable "PM_API_TOKEN_ID" {
   sensitive = true
 }
 
+variable "PM_API_TOKEN_SECRET" {
+  type = string
+  sensitive = true
+}
+
 variable "PUBLIC_SSH_KEY" {
   type = string
   sensitive = true
@@ -28,5 +33,6 @@ variable "CIUSER" {
 provider "proxmox" {
   pm_api_url = var.PM_API_URL
   pm_api_token_id = var.PM_API_TOKEN_ID
+  pm_api_token_secret = var.PM_API_TOKEN_SECRET
   pm_tls_insecure = true
 }
