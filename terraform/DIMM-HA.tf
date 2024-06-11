19920baab75fcb628d3f264d160279ae2ee201fd
Tim Gerritsen <timgerritsen@outlook.com>
2024-06-11 21:17:39 +0000

    Deployment of DIMM-HA

---
+++ terraform/DIMM-HA.tf
@@ -0,0 +1,56 @@
+resource "proxmox_vm_qemu" "DIMM-HA" {
+
+    # General information
+    name = "DIMM-HA"
+    target_node = "DIMM-HV01"
+    vmid = 1006
+    ciuser = var.CIUSER
+    agent = 1
+    onboot = true
+
+    # Cloning information
+    #clone = "TEMP-UBNT-2404-VID10"
+    #full_clone = true
+    #os_type = "cloud-init"
+
+    # Hardware information
+    cpu = "host"
+    sockets = 1
+    cores = 2
+    memory = 4096
+    scsihw = "virtio-scsi-pci"
+    bios = "ovmf"
+
+     # Disk information
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    storage = "local-btrfs"
+                    size = 32
+                    emulatessd = true
+                    discard = true
+                    backup = true
+                    iothread = true
+                    cache = "writethrough"
+                }
+            }
+        }
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local-btrfs"
+                }
+            }
+        }
+    }
+    
+    # Networking information
+    network {
+        model = "virtio"
+        bridge = "vmbr1"
+        tag = 10
+    }
+    ipconfig0 = "ip=10.10.10.6/24,gw=10.10.10.1"
+    sshkeys = var.PUBLIC_SSH_KEY
+}
