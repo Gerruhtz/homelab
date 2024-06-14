f2a66d55a9a000200dfbca35e30899596eee753f
Tim Gerritsen <timgerritsen@outlook.com>
2024-06-14 20:21:35 +0000

    [TEST] Showoff

---
+++ terraform/TEST-MARWIN.tf
@@ -0,0 +1,54 @@
+resource "proxmox_vm_qemu" "TEST-MARWIN" {
+    
+    # General information
+    name = "DIMM-TEST-MARWIN"
+    target_node = "DIMM-HV01"
+    vmid = 2005
+    ciuser = var.CIUSER
+    tags = "tf,ansi"
+
+
+    # Cloning information
+    clone = "TEMP-UBNT-2404-VID20"
+    full_clone = true
+    os_type = "cloud-init"
+
+    # Hardware information
+    cpu = "host"
+    sockets = 1
+    cores = 2
+    memory = 8192
+    scsihw = "virtio-scsi-single"
+
+    # Disk information
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    storage = "local-btrfs"
+                    size = 128
+                    emulatessd = true
+                    discard = true
+                    backup = true
+                    iothread = true
+                }
+            }
+        }
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local-btrfs"
+                }
+            }
+        }
+    }
+    
+    # Networking information
+    network {
+        model = "virtio"
+        bridge = "vmbr1"
+        tag = 20
+    }
+    ipconfig0 = "ip=10.10.20.5/24,gw=10.10.20.1"
+    sshkeys = var.PUBLIC_SSH_KEY
+}
