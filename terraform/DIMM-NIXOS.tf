81665ba36a9c2202c8342d7c8c2a491ff05a3591
Tim Gerritsen <timgerritsen@outlook.com>
2024-08-21 10:40:45 +0000

    Deploy DIMM-NIXOS for testing

---
+++ terraform/DIMM-NIXOS.tf
@@ -0,0 +1,60 @@
+resource "proxmox_vm_qemu" "DIMM-NIXOS" {
+    
+    # General information
+    name = "DIMM-NIXOS"
+    target_node = "DIMM-HV01"
+    vmid = 2020
+    ciuser = var.CIUSER
+    tags = "tf,ansi"
+    onboot = true
+    agent = 1
+
+    # Cloning information
+    # clone = "TEMP-UBNT-2404-VID20"
+    # full_clone = true
+    # os_type = "cloud-init"
+
+    # Hardware information
+    cpu = "host"
+    sockets = 1
+    cores = 2
+    memory = 4096
+    scsihw = "virtio-scsi-single"
+
+    # Disk information
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    storage = "local-btrfs"
+                    size = 64
+                    emulatessd = true
+                    discard = true
+                    backup = true
+                    iothread = true
+                }
+            }
+            scsi1 {
+                cdrom {
+                    iso = "local-btrfs:iso/latest-nixos-gnome-x86-linux.iso"
+                }
+            }
+        }
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local-btrfs"
+                }
+            }
+        }
+    }
+    
+    # Networking information
+    network {
+        model = "virtio"
+        bridge = "vmbr_lan"
+        tag = 20
+    }
+    ipconfig0 = "ip=10.10.20.20/24,gw=10.10.20.1"
+    sshkeys = var.PUBLIC_SSH_KEY
+}
