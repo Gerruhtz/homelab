ff60d161d8428370522f90d3b82bc3110051a40f
Tim Gerritsen <timgerritsen@outlook.com>
2024-09-08 10:34:32 +0000

    Remove Monitor and NixOS, deploy Media

--- terraform/DIMM-MEDIA.tf
+++ terraform/DIMM-MEDIA.tf
@@ -1,24 +1,24 @@
-resource "proxmox_vm_qemu" "DIMM-NIXOS" {
+resource "proxmox_vm_qemu" "DIMM-MEDIA" {
     
     # General information
-    name = "DIMM-NIXOS"
+    name = "DIMM-MEDIA"
     target_node = "DIMM-HV01"
-    vmid = 2020
+    vmid = 1011
     ciuser = var.CIUSER
     tags = "tf,ansi"
     onboot = true
     agent = 1
 
     # Cloning information
-    # clone = "TEMP-UBNT-2404-VID20"
-    # full_clone = true
-    # os_type = "cloud-init"
+    clone = "TEMP-UBNT-2404-VID10"
+    full_clone = true
+    os_type = "cloud-init"
 
     # Hardware information
     cpu = "host"
     sockets = 1
-    cores = 2
-    memory = 4096
+    cores = 4
+    memory = 8192
     scsihw = "virtio-scsi-single"
 
     # Disk information
@@ -27,16 +27,11 @@ resource "proxmox_vm_qemu" "DIMM-NIXOS" {
             scsi0 {
                 disk {
                     storage = "local-btrfs"
-                    size = 64
+                    size = 32
                     emulatessd = true
                     discard = true
                     backup = true
                     iothread = true
-                }
-            }
-            scsi1 {
-                cdrom {
-                    iso = "local-btrfs:iso/latest-nixos-gnome-x86_64-linux.iso"
                 }
             }
         }
@@ -53,8 +48,8 @@ resource "proxmox_vm_qemu" "DIMM-NIXOS" {
     network {
         model = "virtio"
         bridge = "vmbr_lan"
-        tag = 20
+        tag = 10
     }
-    ipconfig0 = "ip=10.10.20.20/24,gw=10.10.20.1"
+    ipconfig0 = "ip=10.10.10.11/24,gw=10.10.10.1"
     sshkeys = var.PUBLIC_SSH_KEY
 }
