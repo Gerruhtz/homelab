67f09fd6352fcbfac78b5962e2a9c9a000d8e482
Tim Gerritsen <timgerritsen@outlook.com>
2024-09-19 15:12:02 +0000

    Upgrade DIMM-MEDIA

--- terraform/DIMM-MEDIA.tf
+++ terraform/DIMM-MEDIA.tf
@@ -16,7 +16,7 @@ resource "proxmox_virtual_environment_vm" "DIMM-MEDIA" {
         cores = 4
         type = "x86-64-v2-AES"
     }
-    memory { dedicated = 4096 }
+    memory { dedicated = 8192 }
     scsi_hardware = "virtio-scsi-single"
     hostpci {
       device = "hostpci0"
